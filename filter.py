8c7c25ad68c86861084ca39e5b090d532e8b795a
walley892 <evanwall@buffalo.edu>
2016-11-05 23:25:31 -0400

    yoooo

--- filter.py
+++ filter.py
@@ -2,44 +2,35 @@ from sklearn.feature_extraction.text import CountVectorizer as CV
 from sklearn.feature_extraction.text import TfidfTransformer as tf
 from sklearn.naive_bayes import MultinomialNB as NB
 
-names = open('articles.txt').read()
+class dankMemeClassifier():
+	def __init__(self, labels_train, data_train):
+		names = open(data_train).read()
+		
+		
+		namesList = names.split('|')
+		
+		self.count_vect = CV(input=u'content')
+		train_counts = self.count_vect.fit_transform(namesList)
+		self.transformer = tf(use_idf = False).fit(train_counts)
+		
 
-namesList = names.split('\n')
-
-
-namesList = namesList[:len(namesList)-1]
-
-count_vect = CV(input=u'content')
-
-train_counts = count_vect.fit_transform(namesList)
-
-
-#returns a matrix based on the file
-
-transformer = tf(use_idf = False).fit(train_counts)
+		tfidf = self.transformer.fit_transform(train_counts)
 
 
-tfidf = transformer.fit_transform(train_counts)
+		a = open(labels_train).read()
+		lb = a.split(',')
+		
+		lb = lb[:len(lb)-1]
+
+		self.clf = NB().fit(tfidf,lb)
+	
+	def classify(self, newpoint):
+		new = self.count_vect.transform(newpoint)
+		new_tfidf = self.transformer.transform(new)
+		pred = self.clf.predict(new_tfidf)
+		return pred
 
 
+c = dankMemeClassifier('training_labels.txt','training_points.txt')
 
-
-#tfidf = transformer.transform(train_counts)
-
-
-a = open('training_labels.txt').read()
-
-labels_train = a.split(',')
-
-clf = NB().fit(tfidf,labels_train)
-
-
-docs = ['A South Korean Man Adopted by Americans Prepares for Deportation']
-
-new = count_vect.transform(docs)
-
-new_tfidf = transformer.transform(new)
-
-pred = clf.predict(new_tfidf)
-
-print pred
+print c.classify(['the'])
